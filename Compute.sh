633b9e4b1dd3e7bd4daf23007d827740ee335ebc
ASR <asinghrathore0@gmail.com>
2021-06-24 09:39:19 +0530

    Added computation solution for UC3

--- Compute.sh
+++ Compute.sh
@@ -4,6 +4,6 @@ read -p "Enter first number : " a
 read -p "Enter second number : " b
 read -p "Enter third number : " c 
 
-result=$(( a + b * c ))
+result=$(( a * b + c ))
 
-echo "Result of $a+$b*$c is : " $result  #Print result
+echo "Result of $a*$b+$c is : " $result  #Print result
