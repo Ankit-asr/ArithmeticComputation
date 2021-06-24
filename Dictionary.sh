d426e0bb03e9b9d76f1aaf73a5c2b434210ed7cb
ASR <asinghrathore0@gmail.com>
2021-06-24 10:27:34 +0530

    Added computaion solutions in Dictionary

---
+++ Dictionary.sh
@@ -0,0 +1,15 @@
+#!/bin/bash 
+
+declare -A results
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c 
+
+results["Result 1"]=$(( a + b * c ))	#Do each computation and store in dictionary		
+results["Result 2"]=$(( a * b + c ))	#Computation 2
+results["Result 3"]=$(( c + a / b ))    #Computation 3
+results["Result 4"]=$(( a % b + c ))    #Computation 4 
+
+echo ${!results[@]}
+echo ${results[@]}
