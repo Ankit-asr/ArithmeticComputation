410c4e6f596b93cbc490c3d95ad733053775124b
ASR <asinghrathore0@gmail.com>
2021-06-24 10:32:41 +0530

    Adds Dictionary elements to Array

---
+++ Array.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+declare -A results
+declare -a array_result
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c 
+
+results["Result 1"]=$(( a + b * c ))	#Do each computation and store in dictionary
+results["Result 2"]=$(( a * b + c ))	#Computation 2
+results["Result 3"]=$(( c + a / b ))   #Computation 3
+results["Result 4"]=$(( a % b + c ))   #Computation 4 
+
+for (( count=0; count<=3; count++))
+do
+
+   array_result[$count]=${results["Result $((count+1))"]} #read values in dictionary into array
+
+done
+
+echo ${array_result[@]}   #print array
