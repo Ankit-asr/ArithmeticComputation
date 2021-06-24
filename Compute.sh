c2bd2885b5e609bcbda5a2fe0fd0e6f3e947db06
ASR <asinghrathore0@gmail.com>
2021-06-24 09:26:22 +0530

    Added computation solution

---
+++ Compute.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter first number : " a
+read -p "Enter second number : " b
+read -p "Enter third number : " c 
+
+result=$(( a + b * c ))
+
+echo "Result of $a+$b*$c is : " $result  #Print result
